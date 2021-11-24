163c3d2f9fe31dd227b0f8381ff2cb95340ee48d
Fabio N. Xavier <fabio.xavier@live.com>
2021-11-24 16:28:20 -0300

    pequena alteração no exe entrada_saida

--- Exercícios/(básico) - entrada_saida.py
+++ Exercícios/(básico) - entrada_saida.py
@@ -1,4 +1,4 @@
 # Programa para ler uma string e retornar uma mensagem.
 
-msn = input('Qual seu nome? ')
+msn = input('Qual seu nome? ')  # A função input recebe dados do usúario no formato string.
 print('Olá', msn)
