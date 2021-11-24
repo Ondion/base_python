db373894848ab4260831f3b22351eeb0f7a405b4
Fabio N. Xavier <fabio.xavier@live.com>
2021-11-24 15:46:11 -0300

    correções menores, exe olá_mundo

---
+++ Exercícios/olá_mundo.py
@@ -0,0 +1,3 @@
+# Programa para imprimir a mensagem “Olá mundo!”
+
+print('Olá mundo!') # No python usamos aspas simples para manipular strings, outro ponto, as bos praticas pedem uma linha vazia ao final do código.
