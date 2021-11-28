a80184477a1daa90d3945d872ca671b040cbc30f
Fabio N. Xavier <fabio.xavier@live.com>
2021-11-28 05:25:58 -0300

    mais execícios

---
+++ Exercícios/(básico) - aluguel_carro.py
@@ -0,0 +1,5 @@
+# Programa que faz o calculo de aluguel entre, Km rodados x tempo de uso. Valor do dia R$: 60,00, valor do Km R$: 0,15.
+
+tempo = int(input('Quantos dias de uso do carro? '))
+kms = int(input('Quantos Kms rodados? '))
+print(f'Você rodou {kms}kms e usou o veiculo por {tempo} dias, o valor a pagar é R$:{(tempo * 60) + (kms * 0.15)}')
