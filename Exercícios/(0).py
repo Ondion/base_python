bf4b3ebd4ef2a0857dcfddc03bd7007ccbcbbb8e
Fabio N. Xavier <fabio.xavier@live.com>
2021-11-28 03:47:13 -0300

    adicao de teorico: operadores aritimeticos e tratamento de string, tambem foi adicionado mais exercicios

--- Exercícios/(0).py
+++
@@ -1,6 +0,0 @@
-# Programa que soma dois números.
-
-a = int(input('número 1: '))
-b = int(input('número 2: '))
-print('A soma entre {0} e {1} é {2}'.format(a, b, a+b))
- 
---
+++ Exercícios/(básico) - numero_antes_depois.py
@@ -0,0 +1,5 @@
+# Programa para localizar o sucessor e antecessor de um número.
+
+numero = int(input('Digite um número: '))
+print('Sucessor: {} e o antecessor: {}'.format(numero+1, numero-1))
+5
