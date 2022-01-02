ad1b19f90acd795ea362b4a4231491cf684795c8
Fabio N. Xavier <fabio.xavier@live.com>
2022-01-02 03:30:13 -0300

    atualizando conteúdo teórico/exercícios, por ser um repositório de estudos, não é versionado

--- Exercícios/(básico) - mini_menu.py
+++ Exercícios/(básico) - mini_menu.py
@@ -1,11 +1,14 @@
+# Programa que simula um mini menu usando o laço while.
 
-
-
-from typing import overload
-import typing_extensions
-
-
-while True:
-    print('Escolha sua opção: \n1 \n2 \n3')
-    str(input(" "))
-    break
+while (True):
+    print('Escolha sua opção: \n1 \n2 \n3 \n4 - Para voltar!')
+    variavel = int(input(""))
+    if (variavel == 1):
+        print('opção 1')
+        break
+    elif (variavel == 2):
+        print('opção 2')
+        break
+    elif (variavel == 3):
+        print('opção 3')
+        break
